80e354d427e33fcb7f90f45fa8928189b8a22890
JarekJJJ <jarek@hkr.net.pl>
2024-09-29 23:51:14 +0200

    Add project files.

---
+++ ServiceWizard/Client/Components/Count.razor.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+
+
+namespace ServiceWizard.Client.Components
+{
+    public partial class Count: ComponentBase
+    {
+        [Inject]
+        private ITestService tService { get; set; }
+
+        private int currentCount = 0;
+        private bool isFailed = false;
+        private string? exMessage = "";
+        private void IncrementCount()
+        {
+            tService.RaiseValue();
+            currentCount = tService.Counter;
+            try
+            {
+                CheckIfCanRaise();
+            }
+            catch (Exception e)
+            {
+                isFailed = true;
+                exMessage = e.Message;
+            }
+        }
+        private void ResetCount()
+        {
+            tService.Counter = 0;
+            currentCount = tService.Counter;
+            isFailed = false;
+        }
+        private void CheckIfCanRaise()
+        {
+            if (currentCount > 5)
+            {
+                throw new InvalidOperationException("count nie może być większy niż 5!");
+            }
+        }
+    }
+}
---
+++ ServiceWizard/Shared/Tasks/Commands/AddTaskVm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceWizard.Shared.Tasks.Commands;
+public class AddTaskVm
+{
+    [Required(ErrorMessage ="Pole wymagane")]
+    public string Name { get; set; }
+    //public string? Description { get; set; }
+    //public DateTime? AddDate { get; set; } = DateTime.Now;
+    //public DateTime? UpdateDate { get; set; }
+    //public DateTime? EndDate { get; set; }
+    //public enum StatusTask {
+    //    [Display(Name ="Nowa")]
+    //    New,
+    //    Rozpoczęta,
+    //    Zakończona } 
+}
